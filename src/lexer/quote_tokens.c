6c25eada8cf60469c70c0f79b7b15860b5cd2695
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2025-12-03 19:08:07 +0100

    fixed unclosed quotes and a leak

--- src/lexer/quote_tokens.c
+++ src/lexer/quote_tokens.c
@@ -7,9 +7,9 @@
 ** Purpose: Check if current character is a quote
 ** Example: is_quote(input[i]) where i is the current position
 */
-int is_quote(char c)
+int	is_quote(char c)
 {
-    return(c == 34 || c == 39);
+	return (c == 34 || c == 39);
 }
 
 /*
@@ -21,18 +21,18 @@ int is_quote(char c)
 ** Returns: index of closing quote, or -1 if not found
 ** Example: find_closing_quote(&input[i + 1], '\'')
 */
-int find_closing_quote(const char *str, char quote)
+int	find_closing_quote(const char *str, char quote)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while(str[i])
-    {
-        if(str[i] == quote)
-            return i;
-        i ++;
-    }
-    return(-1);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == quote)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
 
 /*
@@ -45,26 +45,26 @@ int find_closing_quote(const char *str, char quote)
 ** Returns: allocated string with quotes, or NULL on error
 ** Example: extract_quoted_word(input, &i, &quote_type)
 */
-char *extract_quoted_word(const char *input, int *i, int *quote_type)
+char	*extract_quoted_word(const char *input, int *i, int *quote_type)
 {
-    char    quote;
-    int     close_pos;
-    char    *string;
-    
-    quote = input[*i];
-    if (quote == 39)
-        *quote_type = 1;
-    else
-        *quote_type = 2;
-    close_pos = find_closing_quote(&input[*i + 1], quote);
-    if (close_pos == -1)
-    {
-        printf("minishell: syntax error: unclosed quote\n");
-        return (NULL);
-    }
-    string = ft_substr(input, *i, close_pos + 2);
-    if (!string)
-        return (NULL);
-    *i = *i + close_pos + 2;
-    return (string);
+	char	quote;
+	int		close_pos;
+	char	*string;
+
+	quote = input[*i];
+	if (quote == 39)
+		*quote_type = QUOTE_SINGLE;
+	else
+		*quote_type = QUOTE_DOUBLE;
+	close_pos = find_closing_quote(&input[*i + 1], quote);
+	if (close_pos == -1)
+	{
+		ft_putstr_fd("minishell: syntax error: unclosed quote\n", 2);
+		return (NULL);
+	}
+	string = ft_substr(input, *i + 1, close_pos);
+	if (!string)
+		return (NULL);
+	*i += close_pos + 2;
+	return (string);
 }
