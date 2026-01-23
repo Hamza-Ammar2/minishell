d8cae7503907e74a8d175880525eae527fe38355
Luke Peter Ferenc Pons <lpons@student.42vienna.com>
2026-01-23 22:42:02 +0100

    Refactor ft_exit to pass norminette (split into helper functions)

--- src/exec/builtins3.c
+++ src/exec/builtins3.c
@@ -22,11 +22,50 @@ static int is_numeric(char *str)
     return (1);
 }
 
+static void handle_numeric_exit(char *exit_arg, t_shell *shell)
+{
+    int exit_code;
+
+    exit_code = ft_atoi(exit_arg);
+    free(exit_arg);
+    shell->exit_status = (unsigned char)exit_code;
+    exit((unsigned char)exit_code);
+}
+
+static void handle_exit_error(char *exit_arg, t_shell *shell, int err_type)
+{
+    if (err_type == 0)
+    {
+        fprintf(stderr, "exit: %s: numeric argument required\n", exit_arg);
+        free(exit_arg);
+        shell->exit_status = 2;
+        exit(2);
+    }
+}
+
+static int process_exit_args(t_token **args, t_shell *shell)
+{
+    char *exit_arg;
+
+    if (!args[1])
+        exit(shell->exit_status);
+    exit_arg = expand_str(shell, args[1]->value, args[1]->quote_type);
+    if (!is_numeric(exit_arg))
+        handle_exit_error(exit_arg, shell, 0);
+    if (args[2])
+    {
+        fprintf(stderr, "exit: too many arguments\n");
+        free(exit_arg);
+        shell->exit_status = 1;
+        return (1);
+    }
+    handle_numeric_exit(exit_arg, shell);
+    return (0);
+}
+
 int    ft_exit(t_token **args, t_shell *shell)
 {
     char *cmd;
-    char *exit_arg;
-    int exit_code;
 
     if (!args)
         return (-1);
@@ -34,27 +73,5 @@ int    ft_exit(t_token **args, t_shell *shell)
     if (ft_strcmp(cmd, "exit") != 0)
         return (free(cmd), -1);
     free(cmd);
-    if (args[1])
-    {
-        exit_arg = expand_str(shell, args[1]->value, args[1]->quote_type);
-        if (!is_numeric(exit_arg))
-        {
-            fprintf(stderr, "exit: %s: numeric argument required\n", exit_arg);
-            free(exit_arg);
-            shell->exit_status = 2;
-            exit(2);
-        }
-        if (args[2])
-        {
-            fprintf(stderr, "exit: too many arguments\n");
-            free(exit_arg);
-            shell->exit_status = 1;
-            return (1);
-        }
-        exit_code = ft_atoi(exit_arg);
-        free(exit_arg);
-        shell->exit_status = (unsigned char)exit_code;
-        exit((unsigned char)exit_code);
-    }
-    exit(shell->exit_status);
+    return (process_exit_args(args, shell));
 }
