baecaf53de34d11032602b54ccc8328d35d45bea
Hamza Ammar <haammar@c1r5p3.42vienna.com>
2026-02-07 08:21:57 +0100

    signal fix

--- src/utils/signal_stuff.c
+++ src/utils/signal_stuff.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal_stuff.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpons <lpons@student.42.fr>                +#+  +:+       +#+        */
+/*   By: haammar <haammar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 02:47:21 by lpons             #+#    #+#             */
-/*   Updated: 2026/01/24 02:53:21 by lpons            ###   ########.fr       */
+/*   Updated: 2026/02/07 08:21:47 by haammar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,18 @@ int	sig_hook(void)
 int	init_sig(void)
 {
 	struct sigaction	sa;
+	struct sigaction	sa2;
 
 	memset(&sa, 0, sizeof(sa));
 	sa.sa_handler = handle_sig;
+	sa.sa_handler = SIG_IGN;
 	rl_signal_event_hook = sig_hook;
 	if (sigaction(SIGINT, &sa, NULL) == -1)
+	{
+		perror("sigaction");
+		return (0);
+	}
+	if (sigaction(SIGQUIT, &sa, NULL) == -1)
 	{
 		perror("sigaction");
 		return (0);
